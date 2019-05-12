ab4bdb7ff092decde49b5f109d8b54d007c271c1
siriusGo <904260897@qq.com>
2019-05-12 11:58:12 +0800

    remove RoboCup & Hu

--- RoboCup/myskill/src/GotoPos.h
+++
@@ -1,9 +0,0 @@
-#ifndef GOTOPOS_H
-#define GOTOPOS_H
-#include "utils/PlayerTask.h"
-#include "utils/worldmodel.h"
-#include "utils/PlayerTask.h"
-
-PlayerTask GotoPos_plan(const WorldModel* model, float x, float y, float dir);
-
-#endif
